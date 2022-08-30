1689196f3083586d5ac3e79812f5bb21a2121dd2
yehudz <yehuda.zeytim@gmail.com>
2022-08-30 08:58:59 -0600

    [KTMB-1] Update environment variables
    Install dotenv package and move important credentials to env variables

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 3001
 
 const { Client } = require('pg')
 const client = new Client({
-  user: 'mpixreqjeoodgr',
+  user: process.env.USER,
   host: 'ec2-44-207-126-176.compute-1.amazonaws.com',
   database: 'deibatppg99v10',
-  password: 'a909d1f4e5606f0b410efefcb4a8af2e5260c933ce9e03e23bbbcca04d47ba69',
-  port: 5432,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
   ssl: {
     rejectUnauthorized: false
   }
