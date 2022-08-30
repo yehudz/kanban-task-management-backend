7e1871760b241fa03465231af95ff46debcbfa10
Yehuda Zeytim <yehuda.zeytim@gmail.com>
2022-08-31 02:12:26 +0300

    [KTMB-8] API routes for tasks (#6)
    Create a route to create a task related to a board column
    
    Create a route to get all tasks related to a board column
    
    Update a task
    a. Status to another board column e.g From “To do’s” to “Done”
    b. Name change
    c. Description change
    
    Delete task from board column

--- database.sql
+++ database.sql
@@ -12,3 +12,13 @@ CREATE TABLE boardColumn (
   name VARCHAR(50) NOT NULL,
   CONSTRAINT fk_board FOREIGN KEY(board_id) REFERENCES board(id) on delete cascade
 );
+
+CREATE TABLE task (
+  id uuid DEFAULT uuid_generate_v4 (),
+  PRIMARY KEY (id),
+  board_column_id uuid NOT NULL,
+  task_order INT NOT NULL,
+  title VARCHAR(200) NOT NULL,
+  description VARCHAR(500),
+  CONSTRAINT fk_board_column_id FOREIGN KEY(board_column_id) REFERENCES boardColumn(id) on delete cascade
+);
