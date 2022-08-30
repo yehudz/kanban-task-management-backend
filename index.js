5c7a7e0f4e508a2ea3dd400cae7ba6aba6581007
Yehuda Zeytim <yehuda.zeytim@gmail.com>
2022-08-30 19:18:42 +0300

    [KTMB-2] Create skeleton api routes for boards (#1)
    * [KTMB-2] Get all boards
    
    2. Create new board route
    
    Update env variables to match the database.
    
    * [KTMB-2] Create new board route
    
    1. Create new board route
    
    * [KTMB-2] Get one board
    
    3. Get one board
    
    * [KTMB-2] Update board
    
    4. Update one board
    
    * [KTMB-2] Delete board route
    
    5. Delete board

--- index.js
+++ index.js
@@ -4,25 +4,55 @@ const app = express()
 const port = process.env.PORT || 3001
 
 const { Client } = require('pg')
+
 const client = new Client({
-  user: process.env.USER,
+  user: process.env.DBUSER,
   host: 'ec2-44-207-126-176.compute-1.amazonaws.com',
   database: 'deibatppg99v10',
   password: process.env.PASSWORD,
-  port: process.env.PORT,
+  port: 5432,
   ssl: {
     rejectUnauthorized: false
   }
 })
 client.connect()
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res.rows[0])
-  client.end()
+
+// Create new board
+app.use(express.json())
+app.post('/api/v2/boards', async (req, res)=> {
+  console.log(req.body)
 })
 
-app.get('/getBoards', (req, res)=> {
-  res.send('Hello world')
+
+// Get all boards
+app.get('/api/v2/boards', async (req, res)=> {
+  res.status(200).json({
+    status: "success",
+    data: {
+      board: {name: "First board"}
+    }
+  })
+})
+
+// Get one board
+app.get('/api/v2/boards/:id', async (req, res)=> {
+  const { id } = req.params;
+  console.log(id)
+})
+
+// Update board
+app.use(express.json())
+app.put('/api/v2/boards/:id', async (req, res)=> {
+  const { id } = req.params;
+  console.log(req.body)
+  console.log(id)
+})
+
+// Delete board
+app.delete('/api/v2/boards/:id', async(req, res)=> {
+  const { id } = req.params;
+  console.log(id)
 })
 
 app.listen(port, ()=> {
