cc0b7e6d636a69c358d5047550fa24f76277bc93
yehudz <yehuda.zeytim@gmail.com>
2022-08-30 08:54:01 -0600

    [KTMB-1] Initial setup for backend
    Install all dependencies
    
    Get all credentials for database
    
    Run web server locally

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 3001
 
-app.get('/', (req, res)=> {
+const { Client } = require('pg')
+const client = new Client({
+  user: 'mpixreqjeoodgr',
+  host: 'ec2-44-207-126-176.compute-1.amazonaws.com',
+  database: 'deibatppg99v10',
+  password: 'a909d1f4e5606f0b410efefcb4a8af2e5260c933ce9e03e23bbbcca04d47ba69',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res.rows[0])
+  client.end()
+})
+
+app.get('/getBoards', (req, res)=> {
   res.send('Hello world')
 })
 
