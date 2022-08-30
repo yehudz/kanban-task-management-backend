4bcb6852228d0dd211d4626bdeb3eb8e47b9116b
yehudz <yehuda.zeytim@gmail.com>
2022-08-30 17:11:29 -0600

    [KTMB-8] API routes for tasks
    Create a route to create a task related to a board column
    
    Create a route to get all tasks related to a board column
    
    Update a task
    a. Status to another board column e.g From “To do’s” to “Done”
    b. Name change
    c. Description change
    
    Delete task from board column

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const boards = require('./boards')
 const columns = require('./boardColumns')
+const tasks = require('./tasks')
 // const photos = require('./photos') // Example for future routes
 module.exports = app => {
   app.use('/api/v2/boards', boards),
-  app.use('/api/v2/boardColumns', columns)
+  app.use('/api/v2/boardColumns', columns),
+  app.use('/api/v2/tasks', tasks)
   // app.use('/photos', photos)
   // etc..
 }
