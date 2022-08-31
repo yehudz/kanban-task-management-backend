6a283e6f783c83fa025875e76d283d8e33cbf93d
Yehuda Zeytim <yehuda.zeytim@gmail.com>
2022-08-31 04:30:52 +0300

    [KTMB-7] Create sub tasks inside tasks (#7)
    * [KTMB-7] Create subtask API
    
    Create Subtask API
    
    * [KTMB-7] Get all subtasks from task
    
    API to get all subtasks from task
    
    * [KTMB-7] Update subtask
    
    API route to update subtask
    
    * [KTMB-7] Delete subtask
    
    API route to delete subtask

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const boards = require('./boards')
 const columns = require('./boardColumns')
 const tasks = require('./tasks')
-// const photos = require('./photos') // Example for future routes
+const subtasks = require('./subtasks')
+
 module.exports = app => {
   app.use('/api/v2/boards', boards),
   app.use('/api/v2/boardColumns', columns),
-  app.use('/api/v2/tasks', tasks)
-  // app.use('/photos', photos)
-  // etc..
+  app.use('/api/v2/tasks', tasks),
+  app.use('/api/v2/subtasks', subtasks)
 }
